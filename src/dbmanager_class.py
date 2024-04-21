106134a379775958efbecf7079de4a0efbd4052c
NasirovEF <i@ef-nasirov.ru>
2024-04-21 17:36:38 +0300

    Add all methods in class DBManager

--- src/dbmanager_class.py
+++ src/dbmanager_class.py
@@ -3,27 +3,121 @@ import psycopg2
 
 class DBManager:
     """класс для подключения к БД и работы с ней"""
-    def get_companies_and_vacancies_count(self):
+
+    @staticmethod
+    def get_companies_and_vacancies_count():
         """Получает список всех компаний и количество вакансий у
         каждой компании."""
-        pass
+        conn = psycopg2.connect(host='localhost', database=f'skypro', user='postgres', password='12345')
+        with conn:
+            with conn.cursor() as cur:
+                cur.execute("""SELECT emp_name, COUNT(vac_name) as vacancy_count 
+        FROM employers 
+        INNER JOIN vacancies ON employers.employer_id = vacancies.employer_id 
+        GROUP BY emp_name 
+        ORDER BY vacancy_count DESC""")
+                rows = cur.fetchall()
+                for row in rows:
+                    print(f'Компания: {row[0]}, количество вакансий - {row[1]} шт.')
+        conn.close()
 
-    def get_all_vacancies(self):
+    @staticmethod
+    def get_all_vacancies():
         """Получает список всех вакансий с указанием
         названия компании, названия вакансии и зарплаты и ссылки
         на вакансию."""
-        pass
+        conn = psycopg2.connect(host='localhost', database=f'skypro', user='postgres', password='12345')
+        with conn:
+            with conn.cursor() as cur:
+                cur.execute("""SELECT
+        emp_name, vac_name, salary_from, salary_to, salary_currency, vac_url
+        FROM employers
+        INNER JOIN vacancies ON employers.employer_id = vacancies.employer_id""")
+                rows = cur.fetchall()
+                for row in rows:
+                    print(f'Компания: {row[0]}, {row[1]}, зарплата: {row[2]} - {row[3]} {row[4]}, '
+                          f'ссылка на вакансию: {row[5]}')
+        conn.close()
 
-    def get_avg_salary(self):
+    @staticmethod
+    def get_avg_salary():
         """Получает среднюю зарплату по вакансиям."""
-        pass
+        conn = psycopg2.connect(host='localhost', database=f'skypro', user='postgres', password='12345')
+        with conn:
+            with conn.cursor() as cur:
+                cur.execute("""SELECT AVG(salary_from), AVG(salary_to)
+        FROM vacancies
+        WHERE salary_currency = 'RUR'""")
+                rows = cur.fetchall()
+                for row in rows:
+                    print(f' Средняя зарплата от {round(row[0])} руб. до {round(row[1])} руб.')
 
-    def get_vacancies_with_higher_salary(self):
+        conn.close()
+
+    @staticmethod
+    def get_vacancies_with_higher_salary():
         """Получает список всех вакансий, у которых зарплата
         выше средней по всем вакансиям."""
-        pass
+        conn = psycopg2.connect(host='localhost', database=f'skypro', user='postgres', password='12345')
+        with conn:
+            with conn.cursor() as cur:
+                cur.execute(f"""SELECT emp_name, vac_name, salary_from, salary_to, vac_url
+                FROM vacancies
+                INNER JOIN employers ON vacancies.employer_id = employers.employer_id
+                WHERE salary_from > ANY(SELECT AVG(salary_from) FROM vacancies) AND 
+                salary_to > ANY(SELECT AVG(salary_to) FROM vacancies) AND salary_currency = 'RUR' 
+                ORDER BY salary_from""")
+                rows = cur.fetchall()
+                for row in rows:
+                    print(f'Компания: {row[0]}, {row[1]}, зарплата: {row[2]} - {row[3]} руб., '
+                          f'ссылка на вакансию: {row[4]}')
 
-    def get_vacancies_with_keyword(self):
+        conn.close()
+
+    @staticmethod
+    def get_vacancies_with_keyword(user_input: str):
         """Получает список всех вакансий, в названии
         которых содержатся переданные в метод слова"""
-        pass
+        conn = psycopg2.connect(host='localhost', database=f'skypro', user='postgres', password='12345')
+        with conn:
+            with conn.cursor() as cur:
+                cur.execute(f"""SELECT emp_name, vac_name, salary_from, salary_to, salary_currency, vac_url
+            FROM employers
+            INNER JOIN vacancies ON employers.employer_id = vacancies.employer_id
+            WHERE vac_name LIKE '%{user_input}%'""")
+                rows = cur.fetchall()
+                for row in rows:
+                    print(f'Компания: {row[0]}, {row[1]}, зарплата: {row[2]} - {row[3]} {row[4]}, '
+                          f'ссылка на вакансию: {row[5]}')
+
+        conn.close()
+
+    def repeating_actions(self):
+        """Функция для создания цикла в программе"""
+
+        massage = ("\nДля работы с базой данных для Вас доступны следующие функции:\n"
+                   "Введите '1' для  получения всех компаний и количество вакансий у каждой компании.\n"
+                   "Введите '2' для  получения всех вакансий с указанием названия компании, "
+                   "названия вакансии и зарплаты и ссылки на вакансию.\n"
+                   "Введите '3' для  получения средней зарплаты по вакансиям.\n"
+                   "Введите '4' для  получения всех вакансий, у которых зарплата выше средней по всем вакансиям.\n"
+                   "Введите '5' для  поиска вакансии по заданному слову.\n"
+                   "Введите '0' для  выхода из программы.\n")
+        while True:
+            print(massage)
+            user_input = input("Введите цифру: ")
+            if int(user_input) == 0:
+                break
+            elif int(user_input) == 1:
+                self.get_companies_and_vacancies_count()
+            elif int(user_input) == 2:
+                self.get_all_vacancies()
+            elif int(user_input) == 3:
+                self.get_avg_salary()
+            elif int(user_input) == 4:
+                self.get_vacancies_with_higher_salary()
+            elif int(user_input) == 5:
+                self.get_vacancies_with_keyword(str(input("Введите слово для поиска: ")))
+            else:
+                print("Извините но данная команда отсутствует")
+        print("Работа программы завершена!")
